e7fa6a1bbae05e2b7c250f6b8b8c0497be89856e
marijanikolovski <m.kuzeljevic@gmail.com>
2022-09-30 20:34:15 +0200

    Zadatak 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    $about = 'About us';
+
+    return view('about', compact('about'));
+});
